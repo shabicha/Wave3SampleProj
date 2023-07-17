c852fc8cb23d85cf74ad3aff6be34716d7c9986f
Shabicha Sureshkumar <shabichasureshkumar@gmail.com>
2023-07-17 14:35:35 -0400

    Initial Commit

---
+++ sample32/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  sample32
+//
+//  Created by Shabicha Sureshkumar on 2023-07-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sample32/sample32App.swift
@@ -0,0 +1,17 @@
+//
+//  sample32App.swift
+//  sample32
+//
+//  Created by Shabicha Sureshkumar on 2023-07-17.
+//
+
+import SwiftUI
+
+@main
+struct sample32App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
